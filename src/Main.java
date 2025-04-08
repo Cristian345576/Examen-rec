dfab8fcded57451c9aaec9288990c54b844e2405
Barquero Rodríguez <cristian.barquero@isepceu.es>
2025-04-08 17:11:19 +0200

    cambios prueba Cristian

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+
+/**
+ * @author cristian
+ * @version 3
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Rectangulo rectangulo1 = new Rectangulo(1,"noimbre");
+		Rectangulo rectangulo2 = new Rectangulo(1,"noimbre");
+		Rectangulo rectangulo3 = new Rectangulo(1,"noimbre");
+		Rectangulo rectangulo4 = new Rectangulo(1,"noimbre");
+		
+		imprimirResultados(rectangulo1, rectangulo2, rectangulo3, rectangulo4);
+
+	}
+
+	/**
+	 * Metodo que me sirve para imprimir resultados
+	 * @param rectangulo1 un rectangulo
+	 * @param rectangulo2 w ´reljkga
+	 * @param rectangulo3 gf ewqrhgerwsth
+	 * @param rectangulo4 gtwe5r hertjhewryh
+	 */
+	private static void imprimirResultados(Rectangulo rectangulo1, Rectangulo rectangulo2, Rectangulo rectangulo3,
+			Rectangulo rectangulo4) {
+		System.out.println(rectangulo1);
+		System.out.println(rectangulo2);
+		System.out.println(rectangulo3);
+		System.out.println(rectangulo4);
+	}
+
+}
--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -2,8 +2,30 @@
 public class Rectangulo {
 	private int id;
 	private String nombre;
+	
 	public Rectangulo() {
 		// olwiejhtroiqewr oiqwejrt oiqwe
+	}
+	public Rectangulo(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Rectangulo [id=" + id + ", nombre=" + nombre + "]";
 	}
 	
 
